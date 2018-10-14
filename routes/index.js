1730e5495d4b3fc550b491e2c676d46e2b03c8ec
Andrei Chayeuski <andreichayeuski@gmail.com>
2018-10-14 08:24:08 +0300

    lab 11 completed (bug with index - '/' fixed)

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-	res.render('index', {title : 'Express'});
+	res.send("Hello World! \n CWP-11");
 	console.log("index");
 });
 
